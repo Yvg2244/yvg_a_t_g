e9b39b3da863a8a540cdbae321f00c3d042ba7d1
Yvg2244 <yvgupta24@gmail.com>
2023-08-06 10:57:15 +0530

    build err fix try1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  
+  build: {
+    rollupOptions: {
+      external: [
+        reactIconsBI,
+        // ...other external modules
+      ],
+    },
+  },
+
+  plugins: [react()]
 })
